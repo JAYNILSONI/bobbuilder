d3701a711255e4ae8ea82c39a162ed968233c42a
JAYNILSONI <sonijaynil4@gmail.com>
2022-11-07 03:19:52 +0530

    initial commit

---
+++ scr/calculator.js
@@ -0,0 +1,11 @@
+function reset(){
+    document.getElementById("area").value = "0";
+    document.getElementById("cement").value = "0";
+    document.getElementById("sand").value = "0";
+    document.getElementById("aggregate").value = "0";
+    document.getElementById("steel").value = "0";
+    document.getElementById("paint").value = "0";
+    document.getElementById("bricks").value = "0";
+    document.getElementById("flooring").value = "0";
+    document.getElementById("total").value = "500";
+}
